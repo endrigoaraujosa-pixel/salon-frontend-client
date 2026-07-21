5ac0b4523f1acfb788a71071c83f36a5164932ab
Itamar Nascimento <endrigo.araujosa@gmail.com>
2026-07-21 14:48:58 -0300

    chore: update Docker image naming convention to salon-frontend-client in build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ url=$2
 commit=$(git rev-parse --short HEAD)
 
 
-docker build --build-arg VITE_API_URL=$url -t $user/salon-frontend:$commit .
+docker build --build-arg VITE_API_URL=$url -t $user/salon-frontend-client:$commit .
 docker tag $user/salon-frontend-client:$commit $user/salon-frontend-client:latest
 docker push $user/salon-frontend-client:$commit
 docker push $user/salon-frontend-client:latest
